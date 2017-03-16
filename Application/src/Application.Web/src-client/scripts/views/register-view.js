b4f4b779941fe7244994a92e8b750e3f7779935e
Andrew Mason <andrewmason1@gmail.com>
2017-03-16 13:21:16 -0400

    model fixes and nav

--- Application/src/Application.Web/src-client/scripts/views/register-view.js
+++ Application/src/Application.Web/src-client/scripts/views/register-view.js
@@ -1,4 +1,12 @@
 import React from 'react';
 
 export const RegisterView = React.createClass({
+  render: function(){
+    return (
+      <div>
+        <h2>Sign Up</h2>
+      </div>
+    )
+  }
+
 });
