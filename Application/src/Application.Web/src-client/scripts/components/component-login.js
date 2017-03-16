a430b166ba84df3b6bffe40575e21e297363017c
Andrew Mason <andrewmason1@gmail.com>
2017-03-16 15:43:41 -0400

    user login

--- Application/src/Application.Web/src-client/scripts/components/component-login.js
+++ Application/src/Application.Web/src-client/scripts/components/component-login.js
@@ -1,17 +1,32 @@
-import Backbone from 'backbone';
-import ReactDOM from 'react-dom';
 import React from 'react';
-import {FormComponent} from './component-form.js'
+import {ACTIONS} from '../actions.js'
 
 export const LogInComponent = React.createClass({
 
+  _handleSubmit: function(evt){
+    evt.preventDefault();
+    let formEl = evt.target;
+    console.log(formEl)
+    let emailVal = formEl.emailfield.value
+    let passwordVal = formEl.passwordfield.value
+    ACTIONS.userLogin(emailVal, passwordVal);
+  },
+
   render: function(){
     return (
-      <div>
+      <div className="log-in-container">
         <h1>Bike Safe</h1>
-        <FormComponent/>
+        <div className="input-form">
+          <form onSubmit={this._handleSubmit}>
+            <input type="text" placeholder="email" name="emailfield"></input>
+            <input type="text" placeholder="password" name="passwordfield"></input>
+            <button type="onSubmit">Ride!</button>
+          </form>
+        </div>
         <h2>or</h2>
-        <button type="onClick">Sign Up</button>
+        <div className="input-form">
+          <button type="onClick">Sign Up</button>
+        </div>
       </div>
     )
   }
