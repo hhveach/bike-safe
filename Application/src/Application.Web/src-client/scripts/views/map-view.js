cad0a727478356a43127c7cb3d09f6bde806d910
Hank V <hhveach@yahoo.com>
2017-03-17 15:00:08 -0400

    initial map marker trials

--- Application/src/Application.Web/src-client/scripts/views/map-view.js
+++ Application/src/Application.Web/src-client/scripts/views/map-view.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import GoogleMapReact from 'google-map-react';
+import GoogleMap from 'google-map-react';
 
 export const BasicMapView = React.createClass({
   getInitialState: function(){
@@ -10,29 +10,48 @@ export const BasicMapView = React.createClass({
 
   },
 
-  // _handleClick: function(evt){
-  //   let marker = new google.maps.Marker({
-  //   position: ,
-  //   title:"Hello World!"
-  //   });
-  // },
+  _onClick: function(obj){
+    console.log(obj.lat, obj.lng,);
+    // let newHazardObj = {
+    //   newLat: obj.lat,
+    //   newLong: obj.lng;
+    // };
+    let latitude = obj.lat;
+    let longitude = obj.lng;
+    let marker = new google.maps.Marker({
+      position: {lat: latitude, lng: longitude},
+      // map: {GoogleMap}
+  });
+
+  },
 
   render: function() {
 
+  //   _onClick: function(obj){
+  //     console.log(obj.lat, obj.lng,);
+  //     let latitude = obj.lat;
+  //     let longitude = obj.lng;
+  //     let marker = new google.maps.Marker({
+  //       position: {latitude, longitude},
+  //       map: this.GoogleMap
+  //     });
+  // },
+
     return (<div className="first-map">
-      <GoogleMapReact
+      <GoogleMap
         defaultCenter={this.state.center}
         defaultZoom={this.state.zoom}
         bootstrapURLKeys={{key: 'AIzaSyBGmL06icW_4nOifeu4rxUuEuFzOj2HBjY'}}
         layerTypes={['TrafficLayer', 'TransitLayer']}
-        // onClick={this._handleClick}
+        onClick={this._onClick}
+
       >
         {/* <AnyReactComponent
           lat={59.955413}
           lng={30.337844}
           text={'Kreyser Avrora'}
         /> */}
-      </GoogleMapReact>
+      </GoogleMap>
       </div>
     );
   }
