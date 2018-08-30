7db08928ba02214f7fa0fc01f7a0b8b9bf971e37
Hank V <hhveach@yahoo.com>
2018-08-30 19:52:40 -0400

    still refactoring

--- Application/src/Application.Web/src-client/scripts/components/component-haz-list.js
+++ Application/src/Application.Web/src-client/scripts/components/component-haz-list.js
@@ -1,64 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {ACTIONS} from '../actions.js';
-import {STORE} from '../store.js';
-import {getViewCorners} from '../utils/utils-map.js'
-import { maps } from '@google/maps';
 
-export const HazListComponent = React.createClass({
-  // getDefaultProps: function(){
-  //   let map = this.props.mapEl
-  //   let cornerCoords = getViewCorners(map)
-  //   return ACTIONS.getAllHazards(cornerCoords)
-  // },
-  // componentWillReceiveProps: function(props){
-  //   if (props.mapEl){
-  //
-  //   }
-  // },
-
-  render: function(){
-    console.log('from HazListComponent' , this.props.mapHazards)
-
+export default class HazListComponent extends Component{
+  render = () => {
+    const { mapHazards } = this.props;
     return (
       <div>
-        <HazItems hazItems={this.props.mapHazards}/>
+        <HazItems hazItems={mapHazards}/>
       </div>
     )
   }
+};
 
-})
+class HazItems extends Component {
+  _handleHazDelete = (evt) => {
+    ACTIONS.deleteHaz(evt.target.dataset.hazid);
+  };
 
-const HazItems = React.createClass({
-  _handleHazDelete: function(evt){
-    ACTIONS.deleteHaz(evt.target.dataset.hazid)
-  },
-
-  _createHazardsList: function(haz){
-    let comp = this
-    console.log('from HazItems' , haz)
-    // console.log(this.props.mapEl)
-    let hazMap = haz.map( function(listEl, ind){
+  _createHazardsList = (haz) => {
+    const hazMap = haz.map((listEl, ind) => {
       return (
             <div className="haz-list">
               <li key={ind}>
                 <p>{listEl.type}</p>
-                <i onClick={comp._handleHazDelete} data-hazid={listEl.id} className="fa fa-trash-o" aria-hidden="true"/>
+                <i onClick={this._handleHazDelete} data-hazid={listEl.id} className="fa fa-trash-o" aria-hidden="true"/>
               </li>
             </div>
       )
-    })
-    return hazMap
+    });
+    return hazMap;
+  };
 
-  },
-
-  render: function(){
+  render = () =>  {
+    const { hazItems } = this.props;
     return (
           <div>
             <h1>Your hazard contributions</h1>
             <ul>
-              {this._createHazardsList(this.props.hazItems)}
+              {this._createHazardsList(hazItems)}
             </ul>
           </div>
     )
   }
-})
+};
